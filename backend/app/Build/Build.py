828aac6a7ebd2d2e28ea0b9fe03de760f8fffbf1
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-25 15:50:39 +0200

    Fix GET /Build/{build_id}

--- backend/app/Build/Build.py
+++ backend/app/Build/Build.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 class Build:
     # Get specific build
     # Returns dict{build_id, name, username, cpu, mobo, ram, psu, storage}
@@ -8,27 +9,32 @@ class Build:
         cursor = db.cursor()
         # Note: The word "case" is a reserved keyword in MySQL, so we have escape it with backticks
         sql = '''
-        SELECT build.Build_id as build_id,
-                build.name as name,
-                build.Username as username,
-                CPU.name as cpu,
-                MOBO.name as mobo,
-                RAM.name as ram,
-                PSU.name as psu,
-                Storage.name as storage,
-                `case`.name as `case`
+        SELECT build.Build_id, build.name, build.Username,
+               Build.CPU_id,  CPU.name AS CPU_name,     
+               Build.MOBO_id, MOBO.name AS MOBO_name,   
+               Build.RAM_id,  RAM.name AS RAM_name,     
+               Build.PSU_id,  PSU.name AS PSU_name,     
+               Build.Case_id, `case`.name AS Case_name
         FROM build
             JOIN cpu ON build.CPU_id = cpu.CPU_id
             JOIN MOBO ON build.MOBO_id = MOBO.MOBO_id
             JOIN RAM ON build.RAM_id = RAM.RAM_id
             JOIN PSU ON build.PSU_id = PSU.PSU_id
-            JOIN build_has_storage ON build_has_storage.Build_id = build.Build_id
-            JOIN storage ON build.Build_id = build_has_storage.Build_id
             JOIN `case` ON build.Case_id = `case`.Case_id
         WHERE build.build_id = %s;
         '''
         cursor.execute(sql, (build_id))
         build = cursor.fetchone()
+        sql_storage = '''
+        SELECT storage.Storage_id, storage.name AS Storage_name
+        FROM build_has_storage
+            JOIN storage ON build_has_storage.Storage_id = storage.Storage_id
+        WHERE build_has_storage.Build_id = %s;
+        '''
+        cursor.execute(sql_storage, (build_id))
+        storage = cursor.fetchall()
+        build["storage"] = storage
+        print(build)
         return build
 
     def post(db, new_build):
@@ -39,7 +45,7 @@ class Build:
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
         '''
         cursor.execute(sql, (new_build["name"], new_build["username"], new_build["CPU_id"],
-                             new_build.get("GPU_id", None), #Can be null
+                             new_build.get("GPU_id", None),  # Can be null
                        new_build["MOBO_id"], new_build["RAM_id"], new_build["PSU_id"], new_build["Case_id"], date))
         build_id = cursor.lastrowid
         for storage_id in new_build["storage_ids"]:
--- backend/tests/test.py
+++ backend/tests/test.py
@@ -17,10 +17,10 @@ class BasicTestCase(unittest.TestCase):
     def test_get_build(self):
         response = client.get('/build/1')
         self.assertEqual(response.status_code, 200)
-        expected = {"build_id": 1, "case": "Be Quiet Pure Base 500 FX Gaming Midi Tower", "cpu": "AMD Ryzen 5 5600X", "mobo": "MSI MPG B550 Gaming Plus", "name": "EliteDominance",
-                    "psu": "Be Quiet Dark Power Pro 12", "ram": "G.Skill Ripjaws V", "storage": "Samsung 970 Evo Plus SSD", "username": "PCMasterRace"}
+        expected = {'Build_id': 1, 'name': 'EliteDominance', 'Username': 'PCMasterRace', 'CPU_id': 1, 'CPU_name': 'AMD Ryzen 5 5600X', 'MOBO_id': 0, 'MOBO_name': 'MSI MPG B550 Gaming Plus', 'RAM_id': 4, 'RAM_name': 'G.Skill Ripjaws V',
+                    'PSU_id': 7, 'PSU_name': 'Be Quiet Dark Power Pro 12', 'Case_id': 5, 'Case_name': 'Be Quiet Pure Base 500 FX Gaming Midi Tower', 'storage': [{'Storage_id': 1, 'Storage_name': 'Samsung 870 Evo SSD'}]}
         self.assertEqual(response.json, expected)
-        
+
     def test_post_build(self):
         new_build = {
             "name": "ChristmasBuild",
@@ -42,12 +42,13 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.json["name"], new_build["name"])
         self.assertEqual(response.json["username"], new_build["username"])
         self.assertEqual(response.json["cpu"], "AMD Ryzen 5 5600X")
-        self.assertEqual(response.json["mobo"], "Gigabyte Z790 Gaming X AX (rev. 1.0) Wi-Fi Motherboard")
+        self.assertEqual(
+            response.json["mobo"], "Gigabyte Z790 Gaming X AX (rev. 1.0) Wi-Fi Motherboard")
         self.assertEqual(response.json["ram"], "G.Skill Trident Z5 RGB")
         self.assertEqual(response.json["psu"], "Corsair RMx Series RM850x")
         self.assertEqual(response.json["storage"], "Samsung 970 Evo Plus SSD")
-        self.assertEqual(response.json["case"], "Be Quiet Pure Base 500DX Gaming Midi Tower")
-
+        self.assertEqual(
+            response.json["case"], "Be Quiet Pure Base 500DX Gaming Midi Tower")
 
     def test_get_builds(self):
         response = client.get('/builds')
@@ -67,8 +68,10 @@ class BasicTestCase(unittest.TestCase):
     def test_get_user(self):
         response = client.get('/user/TechEnthusiast42')
         self.assertEqual(response.status_code, 200)
-        expected = {"Username":"TechEnthusiast42","build_ids":[0,8],"email":"tech42@email.com"}
+        expected = {"Username": "TechEnthusiast42",
+                    "build_ids": [0, 8], "email": "tech42@email.com"}
         self.assertEqual(response.json, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
