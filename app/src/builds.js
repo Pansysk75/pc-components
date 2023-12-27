4fbe93b50d90730baefc8cd9b535341892a39ef3
catherinepapad <katerinapapado.01@gmail.com>
2023-12-27 16:00:24 +0200

    Prettify builds display

--- app/src/builds.js
+++ app/src/builds.js
@@ -1,6 +1,10 @@
 import {config} from './config.js';
 
 function fetchBuild() {
+    // Clear previous data
+    document.getElementById("buildDetails").innerHTML = "";
+    document.getElementById("storageDetails").innerHTML = "";
+
     var buildId = document.getElementById('buildId').value;
     var url = config.backendUrl + '/build/' + buildId;
     console.log('Fetching build from ' + url);
@@ -16,7 +20,7 @@ function fetchBuild() {
                 throw new Error('Something went wrong');
             }
         })
-        .then(build => displayBuild(build, "", document.getElementById("buildDetails")))
+        .then(build => displayBuild(build))
         .catch(error => displayError(error));
 }
 
@@ -24,58 +28,8 @@ function fetchBuild() {
 var getBuildButton = document.getElementById('getBuild');
 getBuildButton.addEventListener('click', fetchBuild);
 
-// function displayBuild(build) {
-//     console.log(typeof(build));
-//     var buildDetails = document.getElementById('buildDetails');
-//     buildDetails.innerHTML = JSON.stringify(build, null, 2);
-// }
 
-
-// function displayBuild(build, parentKey = "", container) {
-//     var table = document.createElement("table");
-//     var tbody = document.createElement("tbody");
-
-//     // Create header row
-//     var headerRow = document.createElement("tr");
-//     var th1 = document.createElement("th");
-//     var th2 = document.createElement("th");
-//     th1.textContent = "Key";
-//     th2.textContent = "Value";
-//     headerRow.appendChild(th1);
-//     headerRow.appendChild(th2);
-//     tbody.appendChild(headerRow);
-
-//     console.log(typeof(build));
-//     for (var key in build) {
-//         var row = document.createElement("tr");
-//         var td1 = document.createElement("td");
-//         var td2 = document.createElement("td");
-//         td1.textContent = key;
-//         // td2.textContent = build[key];
-//         if (typeof build[key] === 'object' && build[key] !== null) {
-//             var nestedDiv = document.getElementById("storageDetails");
-//             nestedDiv.classList.add("nested-table"); // Add a class for styling
-//             td2.appendChild(nestedDiv);
-//             displayBuild(build[key], parentKey + key + ".", nestedDiv);
-//         } else {
-//             td2.textContent = build[key];
-//         }
-//         row.appendChild(td1);
-//         row.appendChild(td2);
-//         tbody.appendChild(row);
-//     }
-
-//     table.appendChild(tbody);
-//     container.appendChild(table);
-
-//     // Append the table to the specified div
-//     document.getElementById('buildDetails').appendChild(table);
-
-//     // var buildDetails = document.getElementById('buildDetails');
-//     // buildDetails.innerHTML = JSON.stringify(build, null, 2);
-// }
-
-function displayBuild(build, parentKey = "", container) {
+function displayBuild(build) {
     var table = document.createElement("table");
     var tbody = document.createElement("tbody");
 
@@ -91,32 +45,72 @@ function displayBuild(build, parentKey = "", container) {
 
     // Create rows for each key-value pair
     for (var key in build) {
-        var row = document.createElement("tr");
-        var td1 = document.createElement("td");
-        var td2 = document.createElement("td");
-        td1.textContent = parentKey + key; // Display nested key with its parent key
-        // Check if the value is an object (nested object)
-        if (typeof build[key] === 'object' && build[key] !== null) {
-          // If it's an object, create a new container (div) for the nested table
-          var nestedDiv = document.createElement("div");
-          nestedDiv.classList.add("nested-table"); // Add a class for styling
-          td2.appendChild(nestedDiv);
-          // Recursively call the displayBuild function with the new container
-          displayBuild(build[key], parentKey + key + ".", nestedDiv);
-        } else {
-          td2.textContent = build[key];
-        }
-        row.appendChild(td1);
-        row.appendChild(td2);
-        tbody.appendChild(row);
-      }
-    
-      // Append the tbody to the table
-      table.appendChild(tbody);
-    
-      // Append the table to the specified container
-      container.appendChild(table);
-  }
+      var row = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      
+      if(key !== "storage") {
+        td1.textContent = key;
+        td2.textContent = build[key];
+      }  
+
+      
+      row.appendChild(td1);
+      row.appendChild(td2);
+      tbody.appendChild(row);
+    }
+
+    // Append the tbody to the table
+    table.appendChild(tbody);
+
+    // Append the table to the specified div
+    document.getElementById("buildDetails").appendChild(table);
+
+    displayStorage(build);
+}
+
+function displayStorage(build) {
+    var table = document.createElement("table");
+    var tbody = document.createElement("tbody");
+
+    // Create header row
+    var headerRow = document.createElement("tr");
+    var th1 = document.createElement("th");
+    var th2 = document.createElement("th");
+    th1.textContent = "Storage Component";
+    th2.textContent = "Name";
+    headerRow.appendChild(th1);
+    headerRow.appendChild(th2);
+    tbody.appendChild(headerRow);
+
+    console.log("built is " + typeof(build['storage']));
+    console.log(build['storage']);
+    // Create rows for each key-value pair
+    for (let i=0 ; i<build['storage'].length ; i++){
+        for (var key in build['storage'][i]) {
+        
+            var row = document.createElement("tr");
+            var td1 = document.createElement("td");
+            var td2 = document.createElement("td");
+            
+            
+            td1.textContent = key;
+            td2.textContent = build['storage'][i][key];
+              
+      
+            
+            row.appendChild(td1);
+            row.appendChild(td2);
+            tbody.appendChild(row);
+          }
+    }
+
+    // Append the tbody to the table
+    table.appendChild(tbody);
+
+    // Append the table to the specified div
+    document.getElementById("storageDetails").appendChild(table);
+}
 
 
 function displayError(error) {
