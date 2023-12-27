62ce5b4f1345128cfa69f95098f2606e52c65ea1
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-27 17:30:23 +0200

    Add EJS templated pages

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({ extended: true }));
@@ -20,10 +21,54 @@ router.get('/', (req, res) => {
 });
 
 
-router.get('/builds', (req, res) => {
-    absolutePath = __dirname + '/views/builds.html';
-    res.sendFile(absolutePath);
+// Create html for build, based on id
+router.get('/build/:id', async (req, res) => {
+
+    // Get id from url
+    let id = req.params.id;
+
+    // Fetch build from backend
+    const backendUrl = "http://64.226.122.251:81/build/" + id;
+    let buildData = await fetch(backendUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    // EJS templates are in /templates
+    let templatePath = __dirname + '/templates/build.ejs';
+
+    // Render template with data
+    res.render(templatePath, { build: buildData });
 });
+
+
+router.get('/builds', async (req, res) => {
+
+    // Fetch builds from backend
+    const backendUrl = "http://64.226.122.251:81/builds";
+
+    let buildsData = await fetch(backendUrl)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    // EJS templates are in /templates
+    let templatePath = __dirname + '/templates/builds.ejs';
+
+    // Render template with data
+    res.render(templatePath, { builds: buildsData });
+        
+});
+
 
 router.get('/components', (req, res) => {
     absolutePath = __dirname + '/views/components.html';
--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 const port = process.env.PORT || 8000;
 
 // CORS middleware (similar to your provided script)
