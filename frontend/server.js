1b46e4f8f8d4b98527ea681317adb3a5bbaf91a2
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-28 05:42:30 +0200

    Change page server port

--- frontend/server.js
+++ frontend/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 app.set('view engine', 'ejs');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 // CORS middleware (similar to your provided script)
 app.use((req, res, next) => {
-  const allowedOrigins = ['http:127.0.0.1:81', 'http://localhost:8000', ' http://159.89.215.209:81'];               // not sure about that??
+  const allowedOrigins = ['http:127.0.0.1:81', ' http://159.89.215.209:81'];               // not sure about that??
 // const allowedOrigins = '*';
   const origin = req.headers.origin || '*';
   if (!process.env.DISABLE_XORIGIN || allowedOrigins.indexOf(origin) > -1) {
