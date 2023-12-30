3f13434be3cc5172692791f1f23bfb555ada94b9
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-30 04:34:52 +0200

    /build api: Add popularity metrics (correct commit)

--- api-server/tests/test_Build.py
+++ api-server/tests/test_Build.py
@@ -15,7 +15,9 @@ class TestBuild(unittest.TestCase):
         response = client.get('/build/1')
         self.assertEqual(response.status_code, 200)
         expected = {'Build_id': 1, 'name': 'EliteDominance', 'Username': 'PCMasterRace', 'CPU_id': 1, 'CPU_name': 'AMD Ryzen 5 5600X', 'MOBO_id': 0, 'MOBO_name': 'MSI MPG B550 Gaming Plus', 'RAM_id': 4, 'RAM_name': 'G.Skill Ripjaws V', 'PSU_id': 7,
-                    'PSU_name': 'Be Quiet Dark Power Pro 12', 'Case_id': 5, 'Case_name': 'Be Quiet Pure Base 500 FX Gaming Midi Tower', 'GPU_id': 2, 'GPU_name': 'Gigabyte GeForce RTX 4070', 'date_created': 'Wed, 23 Nov 2016 00:00:00 GMT', 'storage': [{'Storage_id': 1, 'Storage_name': 'Samsung 870 Evo SSD'}]}
+                    'PSU_name': 'Be Quiet Dark Power Pro 12', 'Case_id': 5, 'Case_name': 'Be Quiet Pure Base 500 FX Gaming Midi Tower', 'GPU_id': 2, 'GPU_name': 'Gigabyte GeForce RTX 4070', 'date_created': 'Wed, 23 Nov 2016 00:00:00 GMT', 'storage': [{'Storage_id': 1, 'Storage_name': 'Samsung 870 Evo SSD'}],
+                    'average_rating': 3.0000, 'number_of_ratings': 2, 'times_added_to_favorites': 3}
+
         self.assertEqual(response.json, expected)
 
     def test_post_build(self):
@@ -37,7 +39,8 @@ class TestBuild(unittest.TestCase):
         response = client.get(f'/build/{response.json}')
         build = response.json
         expected = {'Build_id': 45, 'CPU_id': 1, 'CPU_name': 'AMD Ryzen 5 5600X', 'Case_id': 1, 'Case_name': 'Be Quiet Pure Base 500DX Gaming Midi Tower', 'GPU_id': None, 'GPU_name': None, 'MOBO_id': 1,
-                    'MOBO_name': 'Gigabyte Z790 Gaming X AX (rev. 1.0) Wi-Fi Motherboard', 'PSU_id': 1, 'PSU_name': 'Corsair RMx Series RM850x', 'RAM_id': 1, 'RAM_name': 'G.Skill Trident Z5 RGB', 'Username': 'ByteBuster99', 'name': 'ChristmasBuild', 'storage': [{'Storage_id': 1, 'Storage_name': 'Samsung 870 Evo SSD'}, {'Storage_id': 2, 'Storage_name': 'Western Digital Blue'}]}
+                    'MOBO_name': 'Gigabyte Z790 Gaming X AX (rev. 1.0) Wi-Fi Motherboard', 'PSU_id': 1, 'PSU_name': 'Corsair RMx Series RM850x', 'RAM_id': 1, 'RAM_name': 'G.Skill Trident Z5 RGB', 'Username': 'ByteBuster99', 'name': 'ChristmasBuild', 'storage': [{'Storage_id': 1, 'Storage_name': 'Samsung 870 Evo SSD'}, {'Storage_id': 2, 'Storage_name': 'Western Digital Blue'}],
+                    'average_rating': 0.0000, 'number_of_ratings': 0, 'times_added_to_favorites': 0}
         expected["date_created"] = datetime.today().strftime(
             '%a, %d %b %Y 00:00:00 GMT')
         self.assertEqual(build, expected)
@@ -50,7 +53,8 @@ class TestBuild(unittest.TestCase):
         # Check that the first build has the expected values
         build = response.json[0]
         expected = {'Build_id': 0, 'CPU_id': 0, 'CPU_name': 'AMD Ryzen 5 5600G', 'Case_id': 0, 'Case_name': 'Cougar MX410 Mesh-G RGB Gaming Midi Tower', 'GPU_id': 0, 'GPU_name': 'Gigabyte GeForce RTX 3060', 'MOBO_id': 0, 'MOBO_name': 'MSI MPG B550 Gaming Plus', 'PSU_id': 2, 'PSU_name': 'Be Quiet Straight Power 11', 'RAM_id': 2,
-                    'RAM_name': 'Corsair Vengeance RGB Pro', 'Username': 'TechEnthusiast42', 'date_created': 'Mon, 23 Nov 2015 00:00:00 GMT', 'name': 'QuantumDream', 'storage': [{'Build_id': 0, 'Storage_id': 0, 'Storage_name': 'Samsung 970 Evo Plus SSD'}, {'Build_id': 0, 'Storage_id': 2, 'Storage_name': 'Western Digital Blue'}]}
+                    'RAM_name': 'Corsair Vengeance RGB Pro', 'Username': 'TechEnthusiast42', 'date_created': 'Mon, 23 Nov 2015 00:00:00 GMT', 'name': 'QuantumDream', 'storage': [{'Build_id': 0, 'Storage_id': 0, 'Storage_name': 'Samsung 970 Evo Plus SSD'}, {'Build_id': 0, 'Storage_id': 2, 'Storage_name': 'Western Digital Blue'}],
+                    'average_rating': 3.5000, 'number_of_ratings': 2, 'times_added_to_favorites': 6}
         self.assertEqual(build, expected)
 
     def test_get_build_rating(self):
