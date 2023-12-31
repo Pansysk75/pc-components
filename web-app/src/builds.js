dc788a26a3de40393881dd3bb9bdfbec31fe18e8
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-31 18:46:10 +0200

    Add favourites functionality

--- web-app/src/builds.js
+++ web-app/src/builds.js
@@ -1,165 +1,100 @@
 import { config } from "./config.js";
 
-let _favorites = {};
-let _username = "";
+class FavoritesManager {
+    constructor() {
+        this.favorites = {};
+        this.username = "";
+    }
 
-function getFavorites(username) {
-    return new Promise((resolve, reject) => {
-        const backendUrl = config.backendUrl + "/user/" + username + "/favorites";
-
-        // Get favorites from api
-        console.log("Fetching favorites from " + backendUrl);
-        fetch(backendUrl)
-        .then(response => {
+    async fetchAPI(endpoint, options = {}) {
+        const url = `${config.backendUrl}/user/${this.username}/${endpoint}`;
+        try {
+            const response = await fetch(url, options);
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error(`API request failed with status ${response.status}: ${response.statusText}`);
             }
             return response.json();
-        })
-        .then(favorites => {
-            resolve(favorites); // Resolve the promise with the favorites
-        })
-        .catch(error => {
-            reject(error); // Reject the promise with the error
-        });
-    });
-}
-
-// Post favorite to api
-function postFavorite(username, build_id) {
-    return new Promise((resolve, reject) => {
-        const backendUrl = config.backendUrl + "/user/" + username + "/favorites";
-        let new_favorite = {
-            "Build_id": build_id
+        } catch (error) {
+            console.error(`Error during API request to ${url}:`, error);
+            throw error;
         }
-        console.log("Posting favorite to " + backendUrl);
-        fetch(backendUrl, {
+    }
+
+    getFavorites() {
+        return this.fetchAPI('favorites')
+            .then(favoritesArray => {
+                this.favorites = favoritesArray.reduce((map, { Build_id }) => {
+                    map[Build_id] = true;
+                    return map;
+                }, {});
+            });
+    }
+
+    postFavorite(buildId) {
+        return this.fetchAPI('favorites', {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(new_favorite)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            resolve(); // Resolve the promise
-        })
-        .catch(error => {
-            reject(error); // Reject the promise with the error
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ Build_id: buildId })
         });
-    });
-}
+    }
 
-// Delete favorite from api
-function deleteFavorite(username, build_id) {
-    return new Promise((resolve, reject) => {
-        const backendUrl = config.backendUrl + "/user/" + username + "/favorites";
-        let favorite = {
-            "Build_id": build_id
-        }
-        console.log("Deleting favorite from " + backendUrl);
-        fetch(backendUrl, {
+    deleteFavorite(buildId) {
+        return this.fetchAPI('favorites', {
             method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(favorite)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            resolve(); // Resolve the promise
-        })
-        .catch(error => {
-            reject(error); // Reject the promise with the error
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ Build_id: buildId })
         });
-    });
-}
+    }
 
-// When favorite-button is clicked, add/remove build from favorites
-function toggleFavorite(build_id) {
-    return new Promise((resolve, reject) => {
-        if(build_id in _favorites) {
-            deleteFavorite(_username, build_id)
-                .then(() => {
-                    delete _favorites[build_id];
-                    console.log(_favorites);
-                    resolve();
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        }
-        else {
-            postFavorite(_username, build_id)
-                .then(() => {
-                    _favorites[build_id] = true;
-                    console.log(_favorites);
-                    resolve();
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        }
-    });    
-}
+    toggleFavorite(buildId) {
+        const action = buildId in this.favorites ? this.deleteFavorite(buildId) : this.postFavorite(buildId);
 
-function updateFavoriteButton(button) {
-    let build_id = button.value;
-    let icon = button.children[0];
-    if(build_id in _favorites) {
-        icon.style["font-variation-settings"] = '"FILL" 1';
-    }else{
-        icon.style["font-variation-settings"] = '"FILL" 0';
+        return action.then(() => {
+            if (buildId in this.favorites) {
+                delete this.favorites[buildId];
+            } else {
+                this.favorites[buildId] = true;
+            }
+        });
+    }
+
+    updateFavoriteButton(button) {
+        const buildId = button.value;
+        const icon = button.children[0];
+        icon.style["font-variation-settings"] = `"FILL" ${buildId in this.favorites ? 1 : 0}`;
     }
 }
 
+function onFavoriteButtonClick(event) {
+    const buildId = this.value;
 
-function onFavoriteButtonClick() {
-
-    console.log("Favorite button clicked");
-    console.log(_favorites);
-    // Get build_id from favorite button
-    let build_id = this.value;
-
-    // Toggle favorite
-    toggleFavorite(build_id)
+    favoritesManager.toggleFavorite(buildId)
         .then(() => {
-            // Update favorite button
-            updateFavoriteButton(this);
-            console.log(_favorites);
+            favoritesManager.updateFavoriteButton(this);
         })
         .catch(error => {
-            console.error(error);
-        });  
+            console.error(`Error toggling favorite for buildId ${buildId}:`, error);
+        });
 }
 
+const favoritesManager = new FavoritesManager();
+
 document.addEventListener("DOMContentLoaded", () => {
-    _username = document.getElementById("username").value;
-    getFavorites(_username)
-        .then(favorites => {
-            _favorites = favorites. reduce((map, obj) => {
-                map[obj.Build_id] = true;
-                return map;
-            }, {});
-            
-            // Update and add event listener to favorite buttons
-            let favoriteButtons = document.getElementsByClassName("favorite-button");
-            for (let i = 0; i < favoriteButtons.length; i++) {
-                updateFavoriteButton(favoriteButtons[i]);
-                favoriteButtons[i].addEventListener("click", onFavoriteButtonClick);
-            }
+    favoritesManager.username = document.getElementById("username").value;
 
-            console.log(_favorites);
-
+    if (!favoritesManager.username) {
+        return;
+    }
+    
+    favoritesManager.getFavorites()
+        .then(() => {
+            const favoriteButtons = document.getElementsByClassName("favorite-button");
+            Array.from(favoriteButtons).forEach(button => {
+                favoritesManager.updateFavoriteButton(button);
+                button.addEventListener("click", onFavoriteButtonClick);
+            });
         })
         .catch(error => {
-            console.error(error);
+            console.error("Error fetching favorites:", error);
         });
-
-
-
 });
