9c5bf3ae22dc4e36f132165bef4965535b112405
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2024-01-01 00:50:00 +0200

    Add functional delete comment button

--- web-app/src/build.js
+++ web-app/src/build.js
@@ -26,6 +26,52 @@ function submitRating() {
     });
 }
 
-// Hook to submitRating button
-var submitRatingButton = document.getElementById('submitRating');
-submitRatingButton.addEventListener('click', submitRating);
+// Function to delete a rating
+function deleteRating(username, build_id){
+    var url = config.backendUrl + '/build/' + build_id + '/ratings';
+
+    var rating = {
+        "Username": username,
+    }
+
+    console.log('Deleting rating for build ' + build_id + ' by user ' + username);
+    console.log('Url: ' + url);
+    console.log('Rating: ' + JSON.stringify(rating));
+
+    fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(rating)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        window.location.reload();
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+// Wait for the DOM to be loaded
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Hook submitRating button
+    var submitRatingButton = document.getElementById('submitRating');
+    submitRatingButton.addEventListener('click', submitRating);
+
+    // Hook deleteRating buttons
+    var deleteRatingButtons = document.getElementsByClassName('delete-rating');
+    for (const button of deleteRatingButtons) {
+        button.addEventListener('click', function() {
+            // Parent of "rating" class should have a child with class "username"
+            var parent = this.closest('.rating');
+            var username = parent.getElementsByClassName('username')[0].value;
+            var build_id = document.getElementById('build_id').value;
+            deleteRating(username, build_id);
+        });
+    }
+});
+
